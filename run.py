9d364b03acf6eea8921f62a62d844f86ec9b41cd
YanisaW <dmcottoncandy255@gmail.com>
2020-09-16 15:51:24 +0700

    attacut tokenizer

--- run.py
+++ run.py
@@ -1,9 +1,6 @@
 import torch
-from pythainlp.tokenize import sent_tokenize, word_tokenize
+from pythainlp import sent_tokenize, word_tokenize
 print(torch.__version__)
-sentence_1 = "ฉันไปประชุมเมื่อวันที่ 11 มีนาคม"
-sentence_2 = "ข้าราชการได้รับการหมุนเวียนเป็นระยะ \
-    และได้รับมอบหมายให้ประจำในระดับภูมิภาค"
+sentence_1 = "หีบมากมายหลายหีบยกหีบหนี หีบมากมีหนีหีบหนีบหนีหาย เห็นหนีหีบหนีบหนีกันมกกมาย เห็นหีบหายหลายหีบหนีบหนีเอย"
 print("sent_tokenize:", sent_tokenize(sentence_1))
-print("sent_tokenize:", word_tokenize(sentence_1))
-print("no whitespace:", word_tokenize(sentence_1, keep_whitespace=False))
+print("attacut:", word_tokenize(sentence_1, engine="attacut"))
