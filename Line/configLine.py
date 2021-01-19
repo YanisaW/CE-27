6da92608f363f06287214e17540e09d1f4ceee6f
NatnichaTongrod <60010301@kmitl.ac.tth>
2021-01-19 14:55:50 +0700

    add intent

--- Line/configLine.py
+++ Line/configLine.py
@@ -3,3 +3,4 @@ Channel_secret = '5566018cf786ab5ac8866d4a4975d67e'
 Channel_access_token = 'CmYpi4uZyDmJuqeOpwr1FEaQTjilNqe+0xF9U6bjB3RY202OMxo1+1QrVmJm7Y439znfovy4RYuttEte8tHG6QuENfvQSZJvE+8tDI21MTD7vuLTnJEC7PYiqEm4P3iOAdCRD6tsjmY+dkGZ9r4+2AdB04t89/1O/w1cDnyilFU='
 basic_ID = '@807obrap'
 
+
