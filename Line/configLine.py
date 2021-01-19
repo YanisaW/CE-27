66ccfc6536512a027f5bda78c01812ef1279aaf2
NatnichaTongrod <60010301@kmitl.ac.tth>
2021-01-19 17:10:04 +0700

    remove groupID

--- Line/configLine.py
+++ Line/configLine.py
@@ -2,5 +2,6 @@
 Channel_secret = '5566018cf786ab5ac8866d4a4975d67e'
 Channel_access_token = 'CmYpi4uZyDmJuqeOpwr1FEaQTjilNqe+0xF9U6bjB3RY202OMxo1+1QrVmJm7Y439znfovy4RYuttEte8tHG6QuENfvQSZJvE+8tDI21MTD7vuLTnJEC7PYiqEm4P3iOAdCRD6tsjmY+dkGZ9r4+2AdB04t89/1O/w1cDnyilFU='
 basic_ID = '@807obrap'
+groupID = 'Ce425d52ec1df89c844432b3164ff9c88'
 
 
