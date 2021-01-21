aff16af32656731606114c065159bc77457f47a8
NatnichaTongrod <60010301@kmitl.ac.tth>
2021-01-21 17:15:08 +0700

    add contact to admin

--- Line/__init__.py
+++ Line/__init__.py
@@ -26,7 +26,8 @@ def webhook():
         answer = question(message)
         #answer = answer1 +' ID :'+userID
         if answer == "ยิ้มสวยไม่เข้าใจค่ะ ลองถามใหม่อีกครั้งค่ะ":
-            PushMessage(Line.configLine.groupID, message, Line.configLine.Channel_access_token)
+            #PushMessage(Line.configLine.groupID, message, Line.configLine.Channel_access_token)
+            contactAdmin(Reply_token, Line.configLine.Channel_access_token)
         else:
             ReplyMessage(Reply_token, answer, Line.configLine.Channel_access_token)
         return request.json, 200 #success
@@ -68,26 +69,118 @@ def ReplyMessage(Reply_token, TextMessage, Line_Acees_Token):
     r = requests.post(LINE_API, headers=headers, data=data)
     return 200
 
-def PushMessage(id, TextMessage, Line_Acees_Token):
-    LINE_API = 'https://api.line.me/v2/bot/message/push'
+# def PushMessage(id, TextMessage, Line_Acees_Token):
+#     LINE_API = 'https://api.line.me/v2/bot/message/push'
+#
+#     Authorization = 'Bearer {}'.format(Line_Acees_Token) ##ที่ยาวๆ
+#     print(Authorization)
+#     headers = {
+#         'Content-Type': 'application/json; charset=UTF-8',
+#         'Authorization': Authorization
+#     }
+#
+#     data = {
+#         "to": id,
+#         "messages": [{
+#             "type": "text",
+#             "text": TextMessage
+#             }]
+#         }
+#
+#
+#     data = json.dumps(data) ## dump dict >> Json Object
+#     r = requests.post(LINE_API, headers=headers, data=data)
+#     return 200
+
+def contactAdmin(Reply_token, Line_Acees_Token):
+    LINE_API = 'https://api.line.me/v2/bot/message/reply'
 
     Authorization = 'Bearer {}'.format(Line_Acees_Token) ##ที่ยาวๆ
     print(Authorization)
     headers = {
         'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization': Authorization
+        'Authorization':Authorization
     }
 
     data = {
-        "to": id,
-        "messages": [{
-            "type": "text",
-            "text": TextMessage
-            }]
+            "replyToken":Reply_token,
+            "messages":[
+                {
+                    "type": "bubble",
+                    "hero": {
+                        "type": "image",
+                        "url": "https://i.ibb.co/PxRtXZc/image.png",
+                        "size": "full",
+                        "aspectRatio": "2:1",
+                        "aspectMode": "cover",
+                        "action": {
+                            "type": "uri",
+                            "uri": "https://linecorp.com"
+                        },
+                        "margin": "none",
+                        "align": "end",
+                        "gravity": "center",
+                        "offsetTop": "none",
+                        "offsetEnd": "none",
+                        "offsetBottom": "none",
+                        "animated": "false",
+                        "offsetStart": "none"
+                    },
+                    "body": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "spacing": "md",
+                        "action": {
+                            "type": "uri",
+                            "uri": "https://linecorp.com"
+                        },
+                        "contents": [
+                            {
+                                "type": "text",
+                                "text": "ยิ้มสวยไม่เข้าใจค่ะ ",
+                                "size": "lg",
+                                "weight": "bold",
+                                "color": "#232323"
+                            },
+                            {
+                                "type": "box",
+                                "layout": "vertical",
+                                "spacing": "sm",
+                                "contents": []
+                            },
+                            {
+                                "type": "text",
+                                "text": "สามารถติดต่อสอบถามแอดมินได้ค่ะ",
+                                "wrap": "true",
+                                "color": "#636463",
+                                "size": "md"
+                            }
+                        ]
+                    },
+                    "footer": {
+                        "type": "box",
+                        "layout": "vertical",
+                        "contents": [
+                            {
+                                "type": "spacer",
+                                "size": "xxl"
+                            },
+                            {
+                                "type": "button",
+                                "style": "primary",
+                                "color": "#00ac00",
+                                "action": {
+                                    "type": "uri",
+                                    "label": "ติดต่อแอดมิน",
+                                    "uri": "https://lin.ee/4AhDoDQ"
+                                }
+                            }
+                        ]
+                    }
+                }
+            ]
         }
-
 
     data = json.dumps(data) ## dump dict >> Json Object
     r = requests.post(LINE_API, headers=headers, data=data)
     return 200
-
