b07c5ee8e8f264c55b540b140a461243ab425a59
NatnichaTongrod <60010301@kmitl.ac.tth>
2021-01-26 21:06:58 +0700

    add contact admin

--- Line/__init__.py
+++ Line/__init__.py
@@ -95,143 +95,98 @@ def ReplyMessage(Reply_token, TextMessage, Line_Acees_Token):
 def contactAdmin(Reply_token, Line_Acees_Token):
     LINE_API = 'https://api.line.me/v2/bot/message/reply'
 
-    Authorization = 'Bearer {}'.format(Line_Acees_Token) ##ที่ยาวๆ
+    Authorization = 'Bearer {}'.format(Line_Acees_Token)  ##ที่ยาวๆ
     print(Authorization)
     headers = {
         'Content-Type': 'application/json; charset=UTF-8',
-        'Authorization':Authorization
+        'Authorization': Authorization
     }
 
     data = {
-            "replyToken":Reply_token,
-            "messages":[
-                # {
-                #     "type": "bubble",
-                #     "hero": {
-                #         "type": "image",
-                #         "url": "https://i.ibb.co/PxRtXZc/image.png",
-                #         "size": "full",
-                #         "aspectRatio": "2:1",
-                #         "aspectMode": "cover",
-                #         "action": {
-                #             "type": "uri",
-                #             "uri": "https://linecorp.com"
-                #         },
-                #         "margin": "none",
-                #         "align": "end",
-                #         "gravity": "center",
-                #         "offsetTop": "none",
-                #         "offsetEnd": "none",
-                #         "offsetBottom": "none",
-                #         "offsetStart": "none"
-                #     },
-                #     "body": {
-                #         "type": "box",
-                #         "layout": "vertical",
-                #         "spacing": "md",
-                #         "action": {
-                #             "type": "uri",
-                #             "uri": "https://linecorp.com"
-                #         },
-                #         "contents": [
-                #             {
-                #                 "type": "text",
-                #                 "text": "ยิ้มสวยไม่เข้าใจค่ะ ",
-                #                 "size": "lg",
-                #                 "weight": "bold",
-                #                 "color": "#232323"
-                #             },
-                #             {
-                #                 "type": "box",
-                #                 "layout": "vertical",
-                #                 "spacing": "sm",
-                #                 "contents": []
-                #             },
-                #             {
-                #                 "type": "text",
-                #                 "text": "สามารถติดต่อสอบถามแอดมินได้ค่ะ",
-                #                 "color": "#636463",
-                #                 "size": "md"
-                #             }
-                #         ]
-                #     },
-                #     "footer": {
-                #         "type": "box",
-                #         "layout": "vertical",
-                #         "contents": [
-                #             {
-                #                 "type": "spacer",
-                #                 "size": "xxl"
-                #             },
-                #             {
-                #                 "type": "button",
-                #                 "style": "primary",
-                #                 "color": "#00ac00",
-                #                 "action": {
-                #                     "type": "uri",
-                #                     "label": "ติดต่อแอดมิน",
-                #                     "uri": "https://lin.ee/4AhDoDQ"
-                #                 }
-                #             }
-                #         ]
-                #     }
-                # }
-                {
-                    "type": "bubble",
-                    "body": {
-                        "type": "box",
-                        "layout": "vertical",
-                        "spacing": "md",
-                        "action": {
-                            "type": "uri",
-                            "uri": "https://linecorp.com"
-                        },
-                        "contents": [
-                            {
-                                "type": "text",
-                                "text": "ยิ้มสวยไม่เข้าใจค่ะ ",
-                                "size": "lg",
-                                "weight": "bold",
-                                "color": "#232323"
-                            },
-                            {
-                                "type": "box",
-                                "layout": "vertical",
-                                "spacing": "sm",
-                                "contents": []
-                            },
-                            {
-                                "type": "text",
-                                "text": "สามารถติดต่อสอบถามแอดมินได้ค่ะ",
-                                "color": "#636463",
-                                "size": "md"
-                            }
-                        ]
-                    },
-                    "footer": {
-                        "type": "box",
-                        "layout": "vertical",
-                        "contents": [
-                            {
-                                "type": "spacer",
-                                "size": "xxl"
-                            },
-                            {
-                                "type": "button",
-                                "style": "primary",
-                                "color": "#00ac00",
-                                "action": {
-                                    "type": "uri",
-                                    "label": "ติดต่อแอดมิน",
-                                    "uri": "https://lin.ee/4AhDoDQ"
-                                }
-                            }
-                        ]
-                    }
-                }
-            ]
-        }
+        "replyToken": Reply_token,
+        "messages": [
 
-    data = json.dumps(data) ## dump dict >> Json Object
+            {
+                "type": "flex",
+                "altText": "ติดต่อสอบถามแอดมินได้ค่ะ",
+                "contents":
+                    {
+                        "type": "bubble",
+                        "hero": {
+                            "type": "image",
+                            "url": "https://i.ibb.co/PxRtXZc/image.png",
+                            "size": "full",
+                            "aspectRatio": "2:1",
+                            "aspectMode": "cover",
+                            "action": {
+                                "type": "uri",
+                                "uri": "https://linecorp.com"
+                            },
+                            "margin": "none",
+                            "align": "end",
+                            "gravity": "center",
+                            "offsetTop": "none",
+                            "offsetEnd": "none",
+                            "offsetBottom": "none",
+                            "offsetStart": "none"
+                        },
+                        "body": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "spacing": "md",
+                            "action": {
+                                "type": "uri",
+                                "uri": "https://linecorp.com"
+                            },
+                            "contents": [
+                                {
+                                    "type": "text",
+                                    "text": "ยิ้มสวยไม่เข้าใจค่ะ ",
+                                    "size": "lg",
+                                    "weight": "bold",
+                                    "color": "#232323"
+                                },
+                                {
+                                    "type": "box",
+                                    "layout": "vertical",
+                                    "spacing": "sm",
+                                    "contents": []
+                                },
+                                {
+                                    "type": "text",
+                                    "text": "สามารถติดต่อสอบถามแอดมินได้ค่ะ",
+                                    "color": "#636463",
+                                    "size": "md"
+                                }
+                            ]
+                        },
+                        "footer": {
+                            "type": "box",
+                            "layout": "vertical",
+                            "contents": [
+                                {
+                                    "type": "spacer",
+                                    "size": "xxl"
+                                },
+                                {
+                                    "type": "button",
+                                    "style": "primary",
+                                    "color": "#00ac00",
+                                    "action": {
+                                        "type": "uri",
+                                        "label": "ติดต่อแอดมิน",
+                                        "uri": "https://lin.ee/4AhDoDQ"
+                                    }
+                                }
+                            ]
+                        }
+                    }
+
+            }
+
+        ]
+    }
+
+    data = json.dumps(data)  ## dump dict >> Json Object
     r = requests.post(LINE_API, headers=headers, data=data)
     return 200
